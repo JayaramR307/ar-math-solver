37d601c4dd4d3c8fea228e4b62c81167fac252e6
JAYARAM R <61595757+JayaramR307@users.noreply.github.com>
2021-06-03 11:06:35 +0530

    Update APIKey.java

--- app/src/main/java/com/genedev/mathsolver/data/APIKey.java
+++ app/src/main/java/com/genedev/mathsolver/data/APIKey.java
@@ -1,9 +1,5 @@
 package com.genedev.mathsolver.data;
 
-/**
- * Created by raajesharunachalam on 3/25/17.
- */
-
 public class APIKey {
-    public static final String ID = "REW6XA-Q4R64A9G6R";
+    public static final String ID = "Your key";
 }
